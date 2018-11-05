e9821e3020fee9cea8377ba694cb388e63a88cc8
jawwad22 <mjawwadiqbal22@gmail.com>
2018-11-05 10:25:00 -0800

    Add files via upload

---
+++ RemovingLeftFactoring/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemovingLeftFactoring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "A-hgf|hgk";
+            while (true)
+            {
+                RemovingLeftFactoring(input);
+                break;
+            }
+
+
+
+        }
+
+        private static void RemovingLeftFactoring(string input)
+        {
+            string[] k,l,m;
+            string n = "";
+            k=input.Split('-');
+            l = k[1].Split('|');
+            for (int i=0; i < l.Length-1; i++)
+            {
+                for (int j = i; j < l[i + 1].Length; j++)
+                {
+                    if (l[i][j] == l[i + 1][j]) { 
+                    if(!n.Contains(l[i][j])){
+                        n = n + l[i][j];
+                    }
+                    }
+                }
+            }
+            Console.WriteLine(k[0] + "-" + n + "R");
+            string temp = k[1].ToString();
+            temp = temp.Replace(n, "");
+            m = temp.Split('|');
+            Console.Write("R-");
+            for (int i = 0; i < m.Length; i++)
+            {
+                Console.Write(m[i] + "|");
+            }
+            
+            
+        }
+    }
+}
